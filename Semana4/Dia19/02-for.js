9aae2fe94e24dbdaf1e3fc2c802018a920df7749
langelesp <64293290+langelesp@users.noreply.github.com>
2020-07-27 15:48:19 -0500

    Revert "Revert "Merge branch 'master' of https://github.com/langelesp/codigo-virtual-3""
    This reverts commit 4d72dea82caa61705c32a745e651f9c4156ea48c.

---
+++ Semana4/Dia19/02-for.js
@@ -0,0 +1,7 @@
+//Ciclo for
+let numeros = [5, 7, 8, 6, 5, 12, 10];
+console.log(numeros)
+for (let i = 0; i < numeros.length; i++){
+    numeros[i] = numeros[i] * numeros[i];
+}
+console.log(numeros)
---
+++ Semana4/dia17072020/03-typeof.js
@@ -0,0 +1,10 @@
+let nombre;
+console.log(nombre);
+
+let cadena = "un texto cualquiera"
+console.log("cadena");
+
+
+let numero5 = 5;
+console.log(typeof numero5);
+
